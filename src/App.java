9be95faebe579d970717a102c82074791f10bb4d
daylcn <daylane150@gmail.com>
2022-05-16 01:46:18 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+	
+    public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextLong();
+		long L = sc.nextLong();
+		long P = N * L;
+		System.out.println(P);
+
+       
+    }
+	
+}
